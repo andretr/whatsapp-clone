bd8c429ff031bee42de3ccf06e5aeb171445bed8
Andre Ticona - Waverley <andre.ticona@smartbear.com>
2023-05-11 23:08:04 -0400

    renamed components for typescript

--- src/components/ContactListComponent.tsx
+++ src/components/ContactListComponent.tsx
@@ -2,16 +2,16 @@ import { contactList } from "../Data";
 import profilephoto from "../assets/profile/profilephoto.jpeg";
 import SearchContainer from './SearchContainer'
 
-const ContactComponent =(props) => {
+const ContactComponent =(props: { userData: any; }) => {
     const { userData } = props;
     return (
         <div className='flex flex-row px-3 py-4 cursor-pointer bg-white border-b-2 border-solid border-b-[#f2f2f2]'>
             <img className='w-9 h-9 rounded-full' src={userData.profilePic} />
             <div className='flex flex-col w-full my-0 mx-5'>
                 <span className='w-full text-base text-black'>{userData.name}</span>
-                <span className='w-1/5 text-sm mt-1 text-[#000000cc]'>Hello</span>
+                <span className='w-full text-sm mt-1 text-[#000000cc]'>{userData.lastText}</span>
             </div>
-            <span className='w-1/5 text-sm mt-1 text-[#000000cc]'>10:04 PM</span>
+            <span className='w-1/3 text-sm mt-1 text-[#000000cc]'>{userData.lastTextTime}</span>
         </div>
     );
 };
--- src/components/ConversationComponent.tsx
+++ src/components/ConversationComponent.tsx
@@ -7,12 +7,12 @@ const ConversationComponent = () => {
     <div className='flex flex-col h-full flex-[0.75] bg-gray-100'>
         <div className='flex flex-row bg-gray-200 p-4 items-center gap-2.5'>
           <img className='w-10 h-10 rounded-full' src="/profile/pp1.png" />
-          Andre Ticona
+          Julio Fernandez
         </div>
         <MessageContainer />
         <div className='flex bg-gray-200 p-2 items-center bottom-0'>
           <div className="flex flex-row bg-white rounded-2xl w-full py-5">
-            <img className="w-[30px] h-7 opacity-40 cursor-pointer" src={emoji} />
+            <img className="w-[30px] h-7 opacity-40 cursor-pointer pl-2" src={emoji} />
             <SearchInput />
           </div>
         </div>
